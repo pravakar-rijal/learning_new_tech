844df7f061b945481a2a8a34662beb3b9259e6f3
Pravakar Rijal <83540242+pravakar-rijal@users.noreply.github.com>
2026-05-15 01:45:40 +0545

    Add nx monorepo with web app and mobile app

---
+++ nx/vstellar-studio-workspace/apps/vstellar-studio-backend/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'vstellar-studio-backend',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/vstellar-studio-backend',
+};
